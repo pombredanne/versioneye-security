6070f09eec87fe1053a57d2e8020c53a0485180e
reiz <robert.reiz.81@gmail.com>
2015-10-31 14:00:32 +0100

    Refactor the Node.JS sec. crawler for better perf.

--- spec/versioneye/node_security_crawler_spec.rb
+++ spec/versioneye/node_security_crawler_spec.rb
@@ -14,7 +14,7 @@ describe NodeSecurityCrawler do
       worker = Thread.new{ SecurityWorker.new.work }
 
       SecurityProducer.new("node_security")
-      sleep 60
+      sleep 5
 
       worker.exit
 
