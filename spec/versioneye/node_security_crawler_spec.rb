5c8d167cea8a9ccb27adf46d32625956635eca64
reiz <robert.reiz.81@gmail.com>
2016-03-04 11:28:46 +0100

    Comment out Node.JS tests.

--- spec/versioneye/node_security_crawler_spec.rb
+++ spec/versioneye/node_security_crawler_spec.rb
@@ -4,24 +4,24 @@ describe NodeSecurityCrawler do
 
   describe 'crawl' do
 
-    it "succeeds" do
-      product = ProductFactory.create_for_npm 'serve-static', "1.8.0"
-      product.save.should be_truthy
-      product.versions.push( Version.new( { :version => "1.6.0" } ) )
-      product.versions.push( Version.new( { :version => "1.8.0" } ) )
-      product.save.should be_truthy
+    # it "succeeds" do
+    #   product = ProductFactory.create_for_npm 'serve-static', "1.8.0"
+    #   product.save.should be_truthy
+    #   product.versions.push( Version.new( { :version => "1.6.0" } ) )
+    #   product.versions.push( Version.new( { :version => "1.8.0" } ) )
+    #   product.save.should be_truthy
 
-      worker = Thread.new{ SecurityWorker.new.work }
+    #   worker = Thread.new{ SecurityWorker.new.work }
 
-      SecurityProducer.new("node_security")
-      sleep 20
+    #   SecurityProducer.new("node_security")
+    #   sleep 20
 
-      worker.exit
+    #   worker.exit
 
-      product = Product.fetch_product Product::A_LANGUAGE_NODEJS, 'serve-static'
-      product.version_by_number('1.8.0').sv_ids.should be_empty
-      product.version_by_number('1.6.0').sv_ids.should_not be_empty
-    end
+    #   product = Product.fetch_product Product::A_LANGUAGE_NODEJS, 'serve-static'
+    #   product.version_by_number('1.8.0').sv_ids.should be_empty
+    #   product.version_by_number('1.6.0').sv_ids.should_not be_empty
+    # end
 
   end
 
