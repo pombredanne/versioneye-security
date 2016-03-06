b7e2fbb7269785bf53dd4f4047989930a9420cf7
reiz <robert.reiz.81@gmail.com>
2016-03-06 10:45:45 +0100

    Set prefetch to 1
    Always do 1 job at once.

--- lib/versioneye/workers/security_worker.rb
+++ lib/versioneye/workers/security_worker.rb
@@ -5,6 +5,7 @@ class SecurityWorker < Worker
     connection = get_connection
     connection.start
     channel = connection.create_channel
+    channel.prefetch(1)
     queue   = channel.queue("security_crawl", :durable => true)
 
     multi_log " [*] SecurityWorker waiting for messages in #{queue.name}. To exit press CTRL+C"
