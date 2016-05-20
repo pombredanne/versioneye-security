04fb49722fa9fd904e85217786dae906bbaa5cd2
reiz <robert.reiz.81@gmail.com>
2016-05-20 12:00:17 +0200

    Unify the configuration via ENV variables.

---
+++ scripts/set_vars_for_dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# MongoDB
+export DB_PORT_27017_TCP_ADDR=192.168.178.56
+export DB_PORT_27017_TCP_PORT=27017
+
+# ElasticSearch
+export ES_PORT_9200_TCP_ADDR=192.168.178.56
+export ES_PORT_9200_TCP_PORT=9200
+
+# RabbitMQ
+export RM_PORT_5672_TCP_ADDR=192.168.178.56
+export RM_PORT_5672_TCP_PORT=5672
+
+# Memcache
+export MC_PORT_11211_TCP_ADDR=192.168.178.56
+export MC_PORT_11211_TCP_PORT=11211
+
+export RAILS_ENV="development"
+echo "Rails mode: $RAILS_ENV"
